83220382dee9fefe9b4328696ee0c8ebe216e249
Lucie Hélias <lucie.helias@protonmail.com>
2024-09-29 11:54:10 +0200

    create route post for order

--- Routes/order.js
+++ Routes/order.js
@@ -2,19 +2,35 @@ const express = require("express");
 const Order = require("../Models/Order");
 const router = express.Router();
 
-router.post("/orders", async (req, res) => {
+router.post("/order", async (req, res) => {
   try {
-    const products = req.body.products;
+    const { products, totalAmount, status } = req.body;
+
+    if (!products || !totalAmount || !status) {
+      return res
+        .status(400)
+        .json({ message: "Des informations sont manquantes" });
+    }
+
     const newOrder = new Order({
       products: products,
+      totalAmount: totalAmount,
       status: status,
     });
 
     await newOrder.save();
-    res.status(201).json(newOrder);
+
+    return res.status(201).json({
+      message: "Commande créée avec succès",
+      order: newOrder,
+    });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    return res.status(500).json({ message: error.message });
   }
 });
+
+router.get("/orders", async (req, res) => {});
+
+router.put("/orders", async (req, res) => {});
 
 module.exports = router;
