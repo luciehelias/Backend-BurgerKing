bf5ff1582e0c9c4a66e5485bdace82bcdc8570aa
Lucie Hélias <lucie.helias@protonmail.com>
2024-10-06 00:15:54 +0200

    add domain deploy to cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://leroidubiggyburger.netlify.app/",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
 );
